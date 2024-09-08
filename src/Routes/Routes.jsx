a7adf08ce208f63141501eeb855fb50c8f05a676
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-09-08 21:34:56 +0600

    project initiated and setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import Home from '../pages/Home/Home';
+import Main from './../Layout/Main';
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
